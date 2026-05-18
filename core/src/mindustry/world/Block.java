560f37af4d1d51acfcdb415d48e3caf6a71979b1
swx1033-dotcom <swx@gmail.com>
2026-05-18 06:50:37 +0000

    fix PowerGraph compile errors

--- core/src/mindustry/world/Block.java
+++ core/src/mindustry/world/Block.java
@@ -144,8 +144,10 @@ public class Block extends UnlockableContent implements Senseable{
     public boolean unitMoveBreakable;
     /** whether to add this block to brokenblocks */
     public boolean rebuildable = true;
-    /** if true, this logic-related block can only be used with privileged processors (or is one itself) */
+    /** If true, this logic-related block can only be used with privileged processors (or is one itself) */
     public boolean privileged = false;
+    /** Priority of this block for smart power load balancing. Higher values mean the block is more critical. */
+    public int powerPriority = 50;
     /** whether this block can only be placed on water */
     public boolean requiresWater = false;
     /** whether this block can be placed on any liquids, anywhere */
